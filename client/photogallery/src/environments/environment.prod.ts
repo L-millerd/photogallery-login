4595aacda14315b69eed642037f019f80c37b892
L-millerd <lisa.millerd@gmail.com>
2022-07-12 12:50:29 -0700

    production

--- client/photogallery/src/environments/environment.prod.ts
+++ client/photogallery/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
+  production: true,
   name: "Production Env",
   server: "https://seal-app-obde3.ondigitalocean.app/"
 };
--- client/photogallery/src/environments/environment.ts
+++ client/photogallery/src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   name: "Dev Env",
   server: "http://localhost:4400/"
 };
